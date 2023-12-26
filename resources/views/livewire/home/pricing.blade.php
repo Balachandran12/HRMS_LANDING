65b9835d1a15330d5c00cfb747b0e3391e4e39d4
sandd211 <sandhiya@saasforest.com>
2023-12-26 18:53:12 +0530

    add changes

--- resources/views/livewire/home/pricing.blade.php
+++ resources/views/livewire/home/pricing.blade.php
@@ -4,7 +4,7 @@
         costs, that's why our pricing is simple and transparent. Our platform is available at a one-time
         purchase cost, providing you lifelong access to our innovative HR and payroll management solution. </p>
         <div class=" ">
-            <div class="lg:flex gap-x-10 sm:flex sm:justify-center lg:justify-between w-full overflow-y-auto xl:pl-0 lg:pl-0 md:pl-[120px]">
+            <div class="lg:flex gap-x-10 sm:flex sm:justify-center lg:justify-between w-full overflow-y-auto xl:pl-0 lg:pl-0 md:pl-[650px] sm:pl-[620px] ">
                 <livewire:layouts.pricing-card  plans="STANDARD PLAN" img="/assets/icons/standard.svg" price="$200" content="Ideal for small-sized businesses looking to streamline their HR operations" employee="5-55"/>
                 <livewire:layouts.pricing-card  plans="PREMIUM PLAN" img="/assets/icons/premium.svg" price="$350" content="Perfect for growing businesses that need a more robust HR Solution" employee="56-106"/>
                 <livewire:layouts.pricing-card  plans="GOLD PLAN" img="/assets/icons/ultimate.svg" price="$600" content="Designed for medium-sized organizations those in need of in-depth in-sights and data-driven decision-making" employee="107-500"/>
--- resources/views/livewire/layouts/pricing-card.blade.php
+++ resources/views/livewire/layouts/pricing-card.blade.php
@@ -1,7 +1,7 @@
 <div id="pricing">
     <!-- Pricing section start  -->
     
-        <div class="bg-[#F9F5E4] rounded-[20px] pb-10 mt-10 sm:w-[300px] lg:h-[450px] lg:w-[300px] md:w-[400px]  xl:h-[500px]">
+        <div class="bg-[#F9F5E4] rounded-[20px] pb-10 mt-10 sm:w-[280px] lg:h-[450px] lg:w-[300px] md:w-[300px]  xl:h-[500px]">
             <div  style="background-image: url('{{$img}}')" class="bg-contain bg-no-repeat lg:h-[170px] h-[170px] w-full pt-5">
                 <h6 class="font-bold text-center">{{ $plans }}</h6>
                 <h1 class="font-bold text-xl text-center mt-3 px-6">{{$price}}<span class=" text-base font-normal">/month</span></h1>
